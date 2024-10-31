b87bbc6a40a9781efbfaa360c1155299882485c1
MeirZoref <105918888+MeirZoref@users.noreply.github.com>
2024-10-31 12:30:21 +0200

    small fix in LoginWindowController

--- ClientJavaFx/src/login/LoginWindowController.java
+++ ClientJavaFx/src/login/LoginWindowController.java
@@ -12,4 +12,10 @@ public class LoginWindowController {
     public void initialize() {
         sheetWindowController.setLoginWindowController(this);
     }
+
+    @FXML
+    void handleLoginButton() {
+
+    }
+
 }
